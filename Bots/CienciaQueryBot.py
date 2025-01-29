70860f09d718053e4439dc2beb1cd6e2b2791336
compraxio <luisangelacu10@gmail.com>
2025-01-28 19:34:18 -0500

    Importar módulos adicionales para mejorar la funcionalidad del bot

--- Bots/CienciaQueryBot.py
+++ Bots/CienciaQueryBot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from Module import Clasificacion_tiempo_y_peligros, Datos_cientificos_y_consejos, Datos_de_contamincacion, recursos_para_manualidades
 description = 'Una tarea😁'
 intenciones = discord.Intents.default()
 intenciones.message_content = True
